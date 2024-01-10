9f8f4c11c42d91a4479a708b64c66ac7ddecfda0
Yuga Samuel <yugasamuel2002@gmail.com>
2024-01-10 21:13:34 +0700

    Refactor data source to a new file

---
+++ InvokerApp/SpellListViewController+DataSource.swift
@@ -0,0 +1,44 @@
+//
+//  SpellListViewController+DataSource.swift
+//  InvokerApp
+//
+//  Created by Yuga Samuel on 10/01/24.
+//
+
+import UIKit
+
+extension SpellListViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, Spell.ID>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, Spell.ID>
+    
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: Spell.ID) {
+        let spell = spell(withId: id)
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = spell.spellName
+        contentConfiguration.secondaryText = spell.elementsAbbreviation
+        contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
+        
+        contentConfiguration.image = UIImage(named: spell.spellName)
+        contentConfiguration.imageProperties.maximumSize = CGSize(width: 44, height: 44)
+        
+        cell.contentConfiguration = contentConfiguration
+        
+        let backgroundConfiguration = UIBackgroundConfiguration.listGroupedCell()
+        cell.backgroundConfiguration = backgroundConfiguration
+    }
+    
+    func updateSnapshot(reloading ids: [Spell.ID] = []) {
+        var snapshot = Snapshot()
+        snapshot.appendSections([0])
+        snapshot.appendItems(spells.map { $0.id })
+        if !ids.isEmpty {
+            snapshot.reloadItems(ids)
+        }
+        dataSource.apply(snapshot)
+    }
+    
+    func spell(withId id: Spell.ID) -> Spell {
+        let index = spells.indexOfSpell(withId: id)
+        return spells[index]
+    }
+}
--- InvokerApp/SpellListViewController.swift
+++ InvokerApp/SpellListViewController.swift
@@ -10,8 +10,6 @@ import UIKit
 class SpellListViewController: UICollectionViewController {
     var dataSource: DataSource!
     var spells: [Spell] = Spell.sampleData
-    typealias DataSource = UICollectionViewDiffableDataSource<Int, Spell.ID>
-    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, Spell.ID>
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,37 +32,6 @@ class SpellListViewController: UICollectionViewController {
     func listLayout() -> UICollectionViewCompositionalLayout {
         let listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
         return UICollectionViewCompositionalLayout.list(using: listConfiguration)
-    }
-    
-    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: Spell.ID) {
-        let spell = spell(withId: id)
-        var contentConfiguration = cell.defaultContentConfiguration()
-        contentConfiguration.text = spell.spellName
-        contentConfiguration.secondaryText = spell.elementsAbbreviation
-        contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
-        
-        contentConfiguration.image = UIImage(named: spell.spellName)
-        contentConfiguration.imageProperties.maximumSize = CGSize(width: 44, height: 44)
-        
-        cell.contentConfiguration = contentConfiguration
-        
-        let backgroundConfiguration = UIBackgroundConfiguration.listGroupedCell()
-        cell.backgroundConfiguration = backgroundConfiguration
-    }
-    
-    func updateSnapshot(reloading ids: [Spell.ID] = []) {
-        var snapshot = Snapshot()
-        snapshot.appendSections([0])
-        snapshot.appendItems(spells.map { $0.id })
-        if !ids.isEmpty {
-            snapshot.reloadItems(ids)
-        }
-        dataSource.apply(snapshot)
-    }
-    
-    func spell(withId id: Spell.ID) -> Spell {
-        let index = spells.indexOfSpell(withId: id)
-        return spells[index]
     }
 }
 
